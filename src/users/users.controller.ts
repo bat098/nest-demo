501e3de7786aaebc6d0cb10594823c6594668b76
Mateusz Baciak <m.baciak@muchmore.pl>
2024-11-23 22:26:07 +0100

    add user service, change pretteir setting, format all project

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,44 @@
-import { Controller } from "@nestjs/common";
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+  Query,
+} from '@nestjs/common';
 
-@Controller("users")
-export class UsersController {}
+@Controller('users')
+export class UsersController {
+  // GET /USERS
+  @Get()
+  // eslint-disable-next-line
+  findAll(@Query('role') role?: 'INTERN' | 'ADMIN') {
+    return [];
+  }
+
+  //   GET /USERS/:ID
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return { id };
+  }
+
+  //   POST /USERS
+  @Post()
+  create(@Body() user: object) {
+    return user;
+  }
+
+  // Patch /USERS/:ID
+  @Patch(':id')
+  update(@Param('id') id: string, @Body() userUpdate: object) {
+    return { id, ...userUpdate };
+  }
+
+  //   DELETE /USERS/:ID
+  @Delete(':id')
+  delete(@Param('id') id: string) {
+    return { id };
+  }
+}
