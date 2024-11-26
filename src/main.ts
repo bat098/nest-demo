fe8b6036e1056bb8980afd24ba54c5e4c24f8125
Mateusz Baciak <m.baciak@muchmore.pl>
2024-11-26 16:31:30 +0100

    test

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,6 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(process.env.PORT ?? 3000);
 }
-console.log('test1');
+console.log('test2');
 
 bootstrap();
