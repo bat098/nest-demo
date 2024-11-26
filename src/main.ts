238a7cf874402471041c9b2886542d581905f709
Mateusz Baciak <m.baciak@muchmore.pl>
2024-11-26 14:45:45 +0100

    test conventional commit

--- src/main.ts
+++ src/main.ts
@@ -5,5 +5,5 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(process.env.PORT ?? 3000);
 }
-console.log('test');
+console.log('test1');
 bootstrap();
