bbeef08374f6d4b56f096dbd9678c7f775af0079
Mateusz Baciak <m.baciak@muchmore.pl>
2024-12-10 13:21:43 +0100

    add users service

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -9,35 +9,54 @@ import {
   Query,
 } from '@nestjs/common';
 
+import { UsersService } from './users.service';
+
 @Controller('users')
 export class UsersController {
+  constructor(private readonly userService: UsersService) {}
+
   //    GET /users
   @Get()
-  findAll(@Query('role') role?: 'INTERN' | 'ADMIN') {
-    return [];
+  findAll(@Query('role') role?: 'INTER' | 'ENGINNER' | 'ADMIN') {
+    return this.userService.findAll(role);
   }
 
   //   GET /users/:id
   @Get(':id')
   findOne(@Param('id') id: string) {
-    return { id };
+    return this.userService.findOne(+id);
   }
 
   //   POST /users
   @Post()
-  create(@Body() user: object) {
-    return user;
+  create(
+    @Body()
+    user: {
+      name: string;
+      email: string;
+      role: 'INTER' | 'ENGINNER' | 'ADMIN';
+    },
+  ) {
+    return this.userService.create(user);
   }
 
   //   PATCH /users/:id
   @Patch(':id')
-  update(@Param('id') id: string, @Body() userUpdate: object) {
-    return { id, ...userUpdate };
+  update(
+    @Param('id') id: string,
+    @Body()
+    userUpdate: {
+      name?: string;
+      email?: string;
+      role?: 'INTER' | 'ENGINNER' | 'ADMIN';
+    },
+  ) {
+    return this.userService.update(+id, userUpdate);
   }
 
   //   DELETE /users/:id
   @Delete(':id')
   delete(@Param('id') id: string) {
-    return { id };
+    return this.userService.delete(+id);
   }
 }
